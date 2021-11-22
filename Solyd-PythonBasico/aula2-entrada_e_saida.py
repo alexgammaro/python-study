511a39bc1db1f31b33afac1efbd01644dbc2b064
Alexandre Gammaro <alexgammaro@yahoo.com.br>
2021-11-22 01:22:54 -0300

    Inserção de estudos de Python da Solyd

---
+++ Solyd-PythonBasico/aula2-entrada_e_saida.py
@@ -0,0 +1,22 @@
+print("Hello World!")
+print("Segundo print")
+print("Hello World!\nSegundo print")
+print("Hello World!\tSegundo print")
+
+print("==============================================")
+
+nome = "Alexande Gammaro"
+idade = 38
+altura = 1.79
+salario = 2700.32
+tipo_salario = type(salario)
+print(nome)
+print(type(nome))
+print(idade)
+print(type(idade))
+print(altura)
+print(type(altura))
+print(salario)
+print(tipo_salario)
+
+print(nome, "tem", idade, "anos e tem", str(altura) + "M de altura.")
