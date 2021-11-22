b3fb8a0378b0beb169f7eeb6e8d7ac681e7f3d62
Alexandre Gammaro <alexgammaro@yahoo.com.br>
2021-11-22 01:53:52 -0300

    Inserção de estudos de Python da Solyd

--- Solyd-PythonBasico/aula2-entrada_e_saida.py
+++ Solyd-PythonBasico/aula2-entrada_e_saida.py
@@ -5,9 +5,9 @@ print("Hello World!\tSegundo print")
 
 print("==============================================")
 
-nome = "Alexande Gammaro"
-idade = 38
-altura = 1.79
+nome = input("Escreva seu nome: ")
+idade = input("Escreva sua idade: ")
+altura = input("Escreva sua altura: ")
 salario = 2700.32
 tipo_salario = type(salario)
 print(nome)
@@ -18,5 +18,18 @@ print(altura)
 print(type(altura))
 print(salario)
 print(tipo_salario)
-
 print(nome, "tem", idade, "anos e tem", str(altura) + "M de altura.")
+
+print("==============================================")
+
+numero1 = 27
+numero2 = 53
+soma = numero1 + numero2
+sub = numero2 - numero1
+mult = numero1 * numero2
+div = numero2 / numero1
+potencia = 10 ** 2 # potência de 10 elevado a 2
+raiz_quadrada = 16 ** (1/2) # pega a raíz quadrada
+print("A soma é:", soma, "/ A subtracao é:", sub,"/ A multiplicacao é:", mult,"/ A divisao é:", div)
+print("10 elevado a 2 é:", potencia)
+print("Raiz quadrada de 16 é:", raiz_quadrada)
