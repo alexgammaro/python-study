474065cf9d469aef450f4b564ff720a39845d734
Alexandre Gammaro <alexgammaro@yahoo.com.br>
2025-10-22 01:08:34 -0300

    Initial commit

---
+++ DIO/BootCampSantander2025/decryptor.py
@@ -0,0 +1,36 @@
+from cryptography.fernet import Fernet
+import os
+
+#1. Carregar a chave de criptografia dos arquivos
+def carregar_chave():
+    return open("chave.key", "rb").read()
+
+#2. Descriptogravar os arquivos
+def descriptografar_arquivo(arquivo, chave):
+    f = Fernet(chave)
+    with open(arquivo, "rb") as file:
+        dados = file.read()
+        dados_descriptografados = f.decrypt(dados)
+    with open(arquivo, "wb") as file:
+        file.write(dados_descriptografados)
+
+#4. Encontra arquivos para criptografar
+def encontrar_arquivos(diretorio):
+    lista = []
+    for raiz, _, arquivos in os.walk(diretorio):
+        for nome in arquivos:
+            caminho = os.path.join(raiz, nome)
+            if nome != "ransomware.py" and not nome.endswith(".key"):
+                lista.append(caminho)
+    return lista
+
+#5. Execução principal
+def main():
+    chave = carregar_chave()
+    arquivos = encontrar_arquivos("test_files")
+    for arquivo in arquivos:
+        descriptografar_arquivo(arquivo, chave)
+    print("Arquivos restaurados com sucesso!!!")
+
+if __name__=="__main__":
+    main()
